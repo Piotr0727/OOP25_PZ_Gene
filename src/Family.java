c9a446cc19d8c18b89e4f2332f3b167d0fc646a4
Piotr Zajac <piotr0727@gmail.com>
2025-04-28 10:10:44 +0200

    Zad 8 kontenery

--- src/Family.java
+++ src/Family.java
@@ -1,19 +1,23 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Family {
-    private Map<String, Person> people = new HashMap<>();
+    private Map<String, List<Person>> people = new HashMap<>();
     public void add(Person peopleToAdd){
         String newKey = peopleToAdd.getFirst_name()+ " " + peopleToAdd.getLast_name();
         if (people.containsKey(newKey))
             return;
-        people.put(newKey,peopleToAdd);
+        people.put(newKey,List.add(peopleToAdd));
     }
     public void addPeople(Person... peopleToAdd){
         for(Person p : peopleToAdd)
             add(p);
     }
-    public Person get(String key){
+    public List<Person> get(String key){
+        List<Person> temp = new ArrayList<>();
+        //temp.sort();
         return people.get(key);
     }
 
